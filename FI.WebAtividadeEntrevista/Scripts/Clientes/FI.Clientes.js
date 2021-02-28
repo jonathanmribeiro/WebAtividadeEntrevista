f9a1167416b56ccda0bd32dd978fd9eb0b789084
Jonathan Matheus Ribeiro <jonathanmribeiro@outlook.com>
2021-02-28 11:05:56 -0300

    Movida criação do modal de notificacao do JS para HTML

--- FI.WebAtividadeEntrevista/Scripts/Clientes/FI.Clientes.js
+++ FI.WebAtividadeEntrevista/Scripts/Clientes/FI.Clientes.js
@@ -64,25 +64,7 @@ function removerMascara(cpf) {
 }
 
 function ModalDialog(titulo, texto) {
-    var random = Math.random().toString().replace('.', '');
-    var texto = '<div id="' + random + '" class="modal fade">                                                               ' +
-        '        <div class="modal-dialog">                                                                                 ' +
-        '            <div class="modal-content">                                                                            ' +
-        '                <div class="modal-header">                                                                         ' +
-        '                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>         ' +
-        '                    <h4 class="modal-title">' + titulo + '</h4>                                                    ' +
-        '                </div>                                                                                             ' +
-        '                <div class="modal-body">                                                                           ' +
-        '                    <p>' + texto + '</p>                                                                           ' +
-        '                </div>                                                                                             ' +
-        '                <div class="modal-footer">                                                                         ' +
-        '                    <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>             ' +
-        '                                                                                                                   ' +
-        '                </div>                                                                                             ' +
-        '            </div><!-- /.modal-content -->                                                                         ' +
-        '  </div><!-- /.modal-dialog -->                                                                                    ' +
-        '</div> <!-- /.modal -->                                                                                        ';
-
-    $('body').append(texto);
-    $('#' + random).modal('show');
+    $('#MdlNotificacaoTitulo')[0].innerText = titulo;
+    $('#MdlNotificacaoTexto')[0].innerText = texto;
+    $('#MdlNotificacao').modal('show');
 }
